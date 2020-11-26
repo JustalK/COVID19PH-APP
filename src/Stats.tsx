a8c3ad7d8aeb53e15d4cafa6dc3cbc573c6f97ca
Latsuj <justal.kevin@gmail.com>
2020-11-26 14:25:30 +0800

    [FEATURE] Finishing the design for stats

--- src/Stats.tsx
+++ src/Stats.tsx
@@ -23,7 +23,8 @@ export default class Stats extends React.Component<StatsProps, StatsStates> {
 	async componentDidMount(): Promise<void> {
 		const response = await fetch('https://canvasjs.com/data/gallery/react/nifty-stock-price.json');
 		const datas = await response.json();
-		const points = datas.map((d: {x: string, y: string}) => ({
+		const datas_reduced = datas.slice(0, 4);
+		const points = datas_reduced.map((d: {x: string, y: string}) => ({
 			x: new Date(d.x),
 			y: d.y
 		}));
@@ -39,12 +40,26 @@ export default class Stats extends React.Component<StatsProps, StatsStates> {
 			<div id="STATS">
 				<Menu />
 				<div className="wrapper">
-					<span><em>500,000</em> cases in the Philippines</span>
+					<span className="headline">COVID impact in the Philippines</span>
 					<div className="graph">
 						<CanvasJSChart options={canvasConfig}
 							onRef={(ref: any) => this.chart = ref}
 						/>
 					</div>
+					<ul className="resume">
+						<li><em className="color-only up">500,000</em><span>Total Cases</span></li>
+						<li><em className="color-only down">123,000</em><span>Total Women</span></li>
+						<li><em className="color-only up">256,000</em><span>Total Men</span></li>
+					</ul>
+					<ul className="details">
+						<li><span>Number of children affected</span><em className="up">12,030</em></li>
+						<li><span>Number of baby under 6 years old affected</span><em className="down">12,030</em></li>
+						<li><span>Number of older over 60 years old affected</span><em  className="up">1,200</em></li>
+						<li><span>Number of people recovered</span><em  className="up">100,200</em></li>
+						<li><span>Number of people infected</span><em  className="up">200,200</em></li>
+						<li><span>Number of people dead</span><em  className="up">10,200</em></li>
+						<li><span>Area the most affected</span><em  className="up">Batangas</em></li>
+					</ul>
 				</div>
 			</div>
 		);
--- src/config/canvas.config.ts
+++ src/config/canvas.config.ts
@@ -1,28 +1,41 @@
 export const canvasConfig = {
 	height: 260,
 	theme: 'light2',
+	backgroundColor: '#11a389',
 	axisY: {
 		title: '',
 		tickLength: 0,
 		lineThickness: 0,
 		gridThickness: 0,
+		maximum: 9500,
+		minimum: 8500,
 		margin: 0,
 		labelFormatter: function(e: any) {
 			return '';
 		}
 	},
 	axisX: {
-		gridThickness: 100,
-		gridColor: '#11a389',
-		labelFontColor: 'black',
-		tickColor: 'white',
-		lineColor: 'white'
-
+		title: '',
+		tickLength: 0,
+		lineThickness: 0,
+		gridThickness: 0,
+		minimum: 1484000000000,
+		margin: 0,
+		labelFormatter: function(e: any) {
+			return '';
+		}
 	},
 	data: [{
+		indexLabel: " {x} {y} cases",
+		indexLabelPlacement: "auto",
+		indexLabelMaxWidth: 80,
+    	indexLabelWrap: true,
+		indexLabelOrientation: "horizontal",
+		indexLabelFontColor: "white",
+		indexLabelFontSize: "16",
 		lineColor: 'red',
-		color: 'blue',
+		color: 'white',
 		type: 'line',
-		xValueFormatString: 'MMM YYYY'
+		xValueFormatString: 'DD/MM/YY'
 	}]
 }
